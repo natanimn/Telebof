f3cee73b04bd82a40af11d7b641d536d30ff517c
natanimn <natanimcoder@gmail.com>
2024-05-31 16:52:28 +0300

    Added class SetMyName

---
+++ src/main/java/et/telebof/requests/SetMyName.java
@@ -0,0 +1,25 @@
+package et.telebof.requests;
+
+public class SetMyName extends AbstractBaseRequest<SetMyName, Boolean> {
+
+    public SetMyName(RequestSender requestSender){
+        super(requestSender, "setMyName");
+
+    }
+
+    public SetMyName name(String name){
+        add("name", name);
+        return this;
+    }
+
+    public SetMyName languageCode(String languageCode){
+        add("language_code", languageCode);
+        return this;
+    }
+
+
+    @Override
+    public Boolean bind() {
+        return (Boolean) requestSender.makeRequest(this);
+    }
+}
