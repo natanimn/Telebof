483fe27e7e4d0f47502b8240e9ca6b2e3618fc9b
natanimn <natanimcoder@gmail.com>
2024-05-31 16:51:21 +0300

    Added class GetMyName

---
+++ src/main/java/et/telebof/requests/GetMyName.java
@@ -0,0 +1,15 @@
+package et.telebof.requests;
+
+import et.telebof.Util;
+import et.telebof.types.BotName;
+
+public class GetMyName extends AbstractBaseRequest<GetMyName, BotName> {
+    public GetMyName(RequestSender requestSender){
+        super(requestSender, "getMyName");
+    }
+
+    @Override
+    public BotName bind() {
+        return Util.parse(requestSender.makeRequest(this), BotName.class);
+    }
+}
