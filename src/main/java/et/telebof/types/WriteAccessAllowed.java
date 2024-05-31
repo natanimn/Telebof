ceda9c4c5fb34b934eb142f0c65985ebc349672e
natanimn <natanimcoder@gmail.com>
2024-05-31 16:53:58 +0300

    Added field web_app_name

--- src/main/java/et/telebof/types/WriteAccessAllowed.java
+++ src/main/java/et/telebof/types/WriteAccessAllowed.java
@@ -4,5 +4,11 @@ package et.telebof.types;
 import java.io.Serializable;
 
 public class WriteAccessAllowed extends JsonSerializable implements Serializable {
-    // not implemented
+    private String web_app_name;
+
+    public WriteAccessAllowed webAppName(String webAppName){
+        this.web_app_name = webAppName;
+        return this;
+    }
+
 }
