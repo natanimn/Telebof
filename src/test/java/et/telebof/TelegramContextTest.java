c390027acb42eee4f94907e42f6393dbdfdd2e4a
natanimn <natanimcoder@gmail.com>
2024-06-02 16:18:19 +0300

    added test

---
+++ src/test/java/et/telebof/TelegramContextTest.java
@@ -0,0 +1,60 @@
+package et.telebof;
+
+import et.telebof.types.Message;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class TelegramContextTest {
+
+    BotClient bot;
+    long chatId;
+    long userId;
+    String invalidId;
+    String TOKEN;
+
+
+    @Before
+    public void setUp() {
+        TOKEN = System.getenv("TEST_TOKEN");
+        bot = new BotClient(TOKEN);
+        chatId = Long.parseLong(System.getenv("TEST_CHAT_ID"));
+        userId = Long.parseLong(System.getenv("TEST_USER_ID"));
+    }
+
+    void sleep(int secs){
+        try{
+            Thread.sleep(1000L *secs);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void sendMessageTest(){
+        Message message = bot.context.sendMessage(userId, "Hello, World").bind();
+        assertNotNull(message);
+        assertEquals(message.getText(), "Hello, World");
+        sleep(1);
+    }
+
+    @Test
+    public void sendPhotoTest(){
+        File photo = new File("/resources/telegram.png");
+        Message message = bot.context.sendPhoto(userId, photo).caption("Hello, World").bind();
+        assertNotNull(message);
+        assertEquals("Hello, World", message.getCaption());
+        sleep(1);
+    }
+
+    @Test
+    public void forwardMessageTest(){
+        Message message = bot.context.sendMessage(userId, "Trying to forward this message again").bind();
+        Message msg = bot.context.forwardMessage(message.getFrom().getId(), userId, message.getMessageId()).bind();
+        assertNotNull(msg);
+        assertEquals(message.getText(), msg.getText());
+    }
+
+}
---
+++ src/test/java/et/telebof/UtilTest.java
@@ -0,0 +1,53 @@
+package et.telebof;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UtilTest {
+
+    @Test
+    public void isCommand() {
+        assertTrue(Util.isCommand("/start"));
+        assertTrue(Util.isCommand("/start@mybot"));
+        assertFalse(Util.isCommand("!start"));
+    }
+
+    @Test
+    public void extractCommand() {
+        assertEquals("start", Util.extractCommand("/start"));
+        assertEquals("start", Util.extractCommand("/start@mybot"));
+        assertEquals("start", Util.extractCommand("/start hello"));
+    }
+
+    @Test
+    public void extractText() {
+        assertEquals("hello", Util.extractText("/start hello"));
+        assertEquals("hello", Util.extractText("/start@mybot hello"));
+        assertNotEquals("hello", Util.extractText("/start .hello"));
+    }
+
+    @Test
+    public void escapeHtml() {
+        String txt = "<b>Hello, World</b>";
+        assertFalse(Util.escapeHtml(txt).startsWith("<b>"));
+        assertTrue(Util.escapeHtml(txt).startsWith("&lt;"));
+        assertTrue(Util.escapeHtml(txt).endsWith("&gt;"));
+        assertNull(Util.escapeHtml(null));
+        assertEquals("&lt;b&gt;Hello, World&lt;/b&gt;", Util.escapeHtml(txt));
+    }
+
+    @Test
+    public void parseApiResponse() {
+
+    }
+
+    @Test
+    public void parse() {
+
+    }
+
+    @Test
+    public void parseList() {
+
+    }
+}
