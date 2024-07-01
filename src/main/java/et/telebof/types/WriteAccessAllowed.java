1b7bfea2d6b2dd707907c381c3ba06f416fb9a42
natanimn <natanimcoder@gmail.com>
2024-07-01 12:28:45 +0300

    Added fields from_request, from_attachment_menu

--- src/main/java/et/telebof/types/WriteAccessAllowed.java
+++ src/main/java/et/telebof/types/WriteAccessAllowed.java
@@ -5,5 +5,6 @@ import java.io.Serializable;
 
 public class WriteAccessAllowed extends JsonSerializable implements Serializable {
     public String web_app_name;
+    public Boolean from_request, from_attachment_menu;
 
 }
