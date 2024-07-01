e3e9144807f7b1ef74c8943b613789e08679d798
natanimn <natanimcoder@gmail.com>
2024-07-01 12:27:57 +0300

    Added test copyMessage

--- src/test/java/et/telebof/BotContextTest.java
+++ src/test/java/et/telebof/BotContextTest.java
@@ -1,26 +1,23 @@
 package et.telebof;
 
 import et.telebof.types.Message;
+import et.telebof.types.MessageId;
 import org.junit.Before;
 import org.junit.Test;
 import java.io.File;
 
 import static org.junit.Assert.*;
 
-public class BotClientTest {
+public class BotContextTest {
 
     BotClient bot;
-    long chatId;
     long userId;
-    String invalidId;
     String TOKEN;
-
 
     @Before
     public void setUp() {
         TOKEN = System.getenv("TEST_TOKEN");
         bot = new BotClient(TOKEN);
-        chatId = Long.parseLong(System.getenv("TEST_CHAT_ID"));
         userId = Long.parseLong(System.getenv("TEST_USER_ID"));
     }
 
@@ -34,7 +31,7 @@ public class BotClientTest {
 
     @Test
     public void sendMessageTest(){
-        Message message = bot.sendMessage(userId, "Hello, World").exec();
+        Message message = bot.context.sendMessage(userId, "Hello, World").exec();
         assertNotNull(message);
         assertEquals(message.text, "Hello, World");
         sleep(1);
@@ -43,7 +40,7 @@ public class BotClientTest {
     @Test
     public void sendPhotoTest(){
         File photo = new File("src/test/resources/telegram.png");
-        Message message = bot.sendPhoto(userId, photo).caption("Hello, World").exec();
+        Message message = bot.context.sendPhoto(userId, photo).caption("Hello, World").exec();
         assertNotNull(message);
         assertEquals("Hello, World", message.caption);
         sleep(1);
@@ -51,10 +48,19 @@ public class BotClientTest {
 
     @Test
     public void forwardMessageTest(){
-        Message message = bot.sendMessage(userId, "Trying to forward this message again").exec();
-        Message msg = bot.forwardMessage(userId, userId, message.message_id).exec();
+        Message message = bot.context.sendMessage(userId, "Trying to forward this message again").exec();
+        Message msg = bot.context.forward(userId, userId, message.message_id).exec();
         assertNotNull(msg);
         assertEquals(message.text, msg.text);
+        sleep(1);
+    }
+
+    @Test
+    public void copyMessage(){
+        Message message = bot.context.sendMessage(userId, "I will copy this message again").exec();
+        MessageId msg = bot.context.copy(userId, userId, message.message_id).exec();
+        assertNotEquals(msg.message_id, message.message_id);
+        sleep(1);
     }
 
 }
