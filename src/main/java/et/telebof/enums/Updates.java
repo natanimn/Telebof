b9c655d723b8b2a575b786925ef4e8094b29e1a8
natanimn <natanimcoder@gmail.com>
2024-07-01 20:21:22 +0300

    Added field ALL

--- src/main/java/et/telebof/enums/Updates.java
+++ src/main/java/et/telebof/enums/Updates.java
@@ -1,6 +1,7 @@
 package et.telebof.enums;
 
-public enum Updates {
+public enum Updates{
+
     MESSAGE,
     CHANNEL_POST,
     EDITED_MESSAGE,
@@ -14,5 +15,22 @@ public enum Updates {
     PRE_CHECKOUT_QUERY,
     MY_CHAT_MEMBER,
     CHAT_MEMBER,
-    CHAT_JOIN_REQUEST
+    CHAT_JOIN_REQUEST;
+
+    public static final Updates[] ALL = {
+            MESSAGE,
+            CHANNEL_POST,
+            EDITED_MESSAGE,
+            EDITED_CHANNEL_POST,
+            CALLBACK_QUERY,
+            INLINE_QUERY,
+            POLL,
+            POLL_ANSWER,
+            CHOSEN_INLINE_RESULT,
+            SHIPPING_QUERY,
+            PRE_CHECKOUT_QUERY,
+            MY_CHAT_MEMBER,
+            CHAT_MEMBER,
+            CHAT_JOIN_REQUEST
+    };
 }
