f941ba767c8bc11aff83c1c26f4dd1e3333e1643
natanimn <natanimcoder@gmail.com>
2024-07-03 18:53:20 +0300

    Added class DeleteMessages

---
+++ src/main/java/et/telebof/requests/DeleteMessages.java
@@ -0,0 +1,15 @@
+package et.telebof.requests;
+
+public class DeleteMessages extends AbstractBaseRequest<DeleteMessages, Boolean> {
+
+    public DeleteMessages(Object chatId, Integer[] messageIds, RequestSender requestSender) {
+        super(chatId, requestSender, "deleteMessages");
+        add("message_id", messageIds);
+    }
+
+    @Override
+    public Boolean exec() {
+        Object response = requestSender.makeRequest(this);
+        return (Boolean) response;
+    }
+}
