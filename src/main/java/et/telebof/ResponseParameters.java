3c80242475b5747e757f5b49f0e682b7dd5cdc99
natanimn <natanimcoder@gmail.com>
2024-07-03 18:59:31 +0300

    Added class ResponseParameters

---
+++ src/main/java/et/telebof/ResponseParameters.java
@@ -0,0 +1,8 @@
+package et.telebof;
+
+import java.io.Serializable;
+
+public class ResponseParameters implements Serializable {
+    public Long migrate_to_chat_id;
+    public Integer retry_after;
+}
