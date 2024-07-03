5a76effc922559881966b3f13e00185baf4e575a
natanimn <natanimcoder@gmail.com>
2024-07-03 18:59:51 +0300

    Added class SetMessageReaction

---
+++ src/main/java/et/telebof/requests/SetMessageReaction.java
@@ -0,0 +1,24 @@
+package et.telebof.requests;
+
+import et.telebof.types.ReactionType;
+
+import java.util.List;
+
+public class SetMessageReaction extends AbstractBaseRequest<SetMessageReaction, Boolean>{
+    public SetMessageReaction(Object chatId, int message_id, RequestSender requestSender){
+        super(chatId, requestSender, "setMessageReaction");
+    }
+
+    public SetMessageReaction reaction(List<ReactionType> reaction){
+        return add("reaction", reaction);
+    }
+
+    public SetMessageReaction isBig(boolean is_big){
+        return add("is_big", is_big);
+    }
+
+    @Override
+    public Boolean exec() {
+        return (Boolean) requestSender.makeRequest(this);
+    }
+}
