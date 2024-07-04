06b41a4dd4fdfc8e76852b1563daec36a28269dc
natanimn <natanimcoder@gmail.com>
2024-07-04 23:56:15 +0300

    Fix typing

--- src/main/java/et/telebof/types/UsersShared.java
+++ src/main/java/et/telebof/types/UsersShared.java
@@ -5,10 +5,5 @@ import java.util.List;
 
 public class UsersShared implements Serializable {
     public int request_id;
-    public List<User> users;
-
-    public UsersShared(int request_id, List<User> users){
-        this.request_id = request_id;
-        this.users = users;
-    }
+    public List<SharedUser> users;
 }
