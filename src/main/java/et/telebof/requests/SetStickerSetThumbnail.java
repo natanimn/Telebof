486759fae85616434459dea1e490462878a747ca
natanimn <natanimcoder@gmail.com>
2024-07-04 23:58:47 +0300

    Added parameter format

--- src/main/java/et/telebof/requests/SetStickerSetThumbnail.java
+++ src/main/java/et/telebof/requests/SetStickerSetThumbnail.java
@@ -4,19 +4,21 @@ import java.io.File;
 
 public class SetStickerSetThumbnail extends AbstractBaseRequest<SetStickerSetThumbnail, Boolean> {
 
-    public SetStickerSetThumbnail(String name, long userId, File thumbnail, RequestSender requestSender) {
+    public SetStickerSetThumbnail(String name, long userId, File thumbnail, String format, RequestSender requestSender) {
         super(requestSender, "setStickerSetThumbnail");
         add("name", name);
         add("user_id", userId);
         add("thumbnail", thumbnail);
+        add("format", format);
         setContentType(MediaContentType.PHOTO);
     }
 
-    public SetStickerSetThumbnail(String name, long userId, String thumbnail, RequestSender requestSender) {
+    public SetStickerSetThumbnail(String name, long userId, String thumbnail, String format,  RequestSender requestSender) {
         super(requestSender, "setStickerSetThumbnail");
         add("name", name);
         add("user_id", userId);
         add("thumbnail", thumbnail);
+        add("format", format);
     }
 
     @Override
