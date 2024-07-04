9ebd3e7cc65009881424d5dca21f6201891388e0
natanimn <natanimcoder@gmail.com>
2024-07-05 00:03:33 +0300

    Added fields title, request_name, request_username, request_photo and their methods

--- src/main/java/et/telebof/types/KeyboardButtonRequestChat.java
+++ src/main/java/et/telebof/types/KeyboardButtonRequestChat.java
@@ -5,7 +5,8 @@ import java.util.TreeMap;
 
 public class KeyboardButtonRequestChat extends JsonSerializable {
     private ChatAdministratorRights user_administrator_rights, bot_administrator_rights;
-    private boolean bot_is_member, chat_is_forum, chat_has_username, chat_is_created, chat_is_channel;
+    private Boolean bot_is_member, chat_is_forum, chat_has_username, chat_is_created, chat_is_channel, request_name,
+    request_title, request_username, request_photo;
     private Integer request_id;
 
     public KeyboardButtonRequestChat(int requestId) {
@@ -44,6 +45,26 @@ public class KeyboardButtonRequestChat extends JsonSerializable {
 
     public KeyboardButtonRequestChat botIsMember(boolean botIsMember) {
         this.bot_is_member = botIsMember;
+        return this;
+    }
+
+    public  KeyboardButtonRequestChat requestName(boolean request_name){
+        this.request_name = request_name;
+        return this;
+    }
+
+    public  KeyboardButtonRequestChat requestTitle(boolean request_title){
+        this.request_title = request_title;
+        return this;
+    }
+
+    public  KeyboardButtonRequestChat requestUsername(boolean request_username){
+        this.request_username = request_username;
+        return this;
+    }
+
+    public  KeyboardButtonRequestChat requestPhoto(boolean request_photo){
+        this.request_photo = request_photo;
         return this;
     }
 
