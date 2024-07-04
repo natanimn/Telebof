ceb6fb28b9f79d374315f4c3c5472a80c6976644
natanimn <natanimcoder@gmail.com>
2024-07-05 00:43:56 +0300

    Added class ReplaceStickerInSet

--- src/main/java/et/telebof/requests/ReplaceStickerInSet.java
+++ src/main/java/et/telebof/requests/ReplaceStickerInSet.java
@@ -1,16 +1,17 @@
 package et.telebof.requests;
 
-import java.io.File;
+import et.telebof.types.InputSticker;
+
+
 
 public class ReplaceStickerInSet extends AbstractBaseRequest<ReplaceStickerInSet, Boolean>{
 
-    public ReplaceStickerInSet(long user_id, String name, String old_sticker, File sticker, RequestSender requestSender){
+    public ReplaceStickerInSet(long user_id, String name, String old_sticker, InputSticker sticker, RequestSender requestSender){
         super(requestSender, "replaceStickerInSet");
         add("user_id", user_id);
         add("name", name);
         add("old_sticker", old_sticker);
         add("sticker", sticker);
-        this.setContentType(MediaContentType.PHOTO);
     }
 
     @Override
