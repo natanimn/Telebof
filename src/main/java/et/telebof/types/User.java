8b9c79cdd811360359a9e43b77a18ea09becb57c
natanimn <natanimcoder@gmail.com>
2024-07-04 23:56:42 +0300

    Added field can_connect_to_business

--- src/main/java/et/telebof/types/User.java
+++ src/main/java/et/telebof/types/User.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class User extends JsonSerializable implements Serializable {
     public Long id;
     public Boolean is_bot, is_premium, added_to_attachment_menu, can_join_groups,
-            can_read_all_group_messages, supports_inline_queries;
+            can_read_all_group_messages, supports_inline_queries, can_connect_to_business;
     public String first_name, last_name, language_code, username;
 
     public String mention(){
