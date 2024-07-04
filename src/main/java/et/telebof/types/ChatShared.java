886a782e229f3d57aab6efa43e717e318240ef41
natanimn <natanimcoder@gmail.com>
2024-07-04 23:51:48 +0300

    Added fields title, username, photo

--- src/main/java/et/telebof/types/ChatShared.java
+++ src/main/java/et/telebof/types/ChatShared.java
@@ -1,9 +1,11 @@
 package et.telebof.types;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class ChatShared extends JsonSerializable implements Serializable {
     public Integer request_id;
     public Long chat_id;
-
+    public String title, username;
+    public List<PhotoSize> photo;
 }
