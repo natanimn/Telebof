071ad97abc005d37de1360d53047ede7fda947c0
natanimn <natanimcoder@gmail.com>
2024-07-04 23:46:40 +0300

    Added class BusinessOpeningHoursInterval

---
+++ src/main/java/et/telebof/types/BusinessOpeningHoursInterval.java
@@ -0,0 +1,7 @@
+package et.telebof.types;
+
+import java.io.Serializable;
+
+public class BusinessOpeningHoursInterval implements Serializable {
+    public Integer opening_minute, closing_minute;
+}
