d4852f48cb295401ea71ef3d97f2038732f7c353
natanimn <natanimcoder@gmail.com>
2024-07-05 00:02:18 +0300

    Added fields request_name, request_username, request_photo and their methods

--- src/main/java/et/telebof/types/KeyboardButtonRequestUsers.java
+++ src/main/java/et/telebof/types/KeyboardButtonRequestUsers.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 
 public class KeyboardButtonRequestUsers implements Serializable {
     private Long request_id;
-    private Boolean user_is_bot, user_is_premium;
+    private Boolean user_is_bot, user_is_premium, request_name, request_username, request_photo;
     private Integer max_quantity;
 
     public KeyboardButtonRequestUsers(long requestId) {
@@ -24,6 +24,21 @@ public class KeyboardButtonRequestUsers implements Serializable {
 
     public KeyboardButtonRequestUsers maxQuantity(Integer max_quantity){
         this.max_quantity = max_quantity;
+        return this;
+    }
+
+    public  KeyboardButtonRequestUsers requestName(boolean request_name){
+        this.request_name = request_name;
+        return this;
+    }
+
+    public  KeyboardButtonRequestUsers requestUsername(boolean request_username){
+        this.request_username = request_username;
+        return this;
+    }
+
+    public  KeyboardButtonRequestUsers requestPhoto(boolean request_photo){
+        this.request_photo = request_photo;
         return this;
     }
 
