8362f91682c83c2beab54b110ddc92b42e79a9ef
natanimn <natanimcoder@gmail.com>
2024-07-05 12:28:22 +0300

    Added class BackgroundType

---
+++ src/main/java/et/telebof/types/BackgroundType.java
@@ -0,0 +1,9 @@
+package et.telebof.types;
+
+public class BackgroundType {
+    public String type, theme_name;
+    public BackgroundFill fill;
+    public Integer dark_theme_dimming, intensity;
+    public Document document;
+    Boolean is_blurred, is_moving, is_inverted;
+}
