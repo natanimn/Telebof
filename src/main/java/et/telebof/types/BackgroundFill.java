cb0a0c2665cd68efba3bccb97b4d5d834bbecae1
natanimn <natanimcoder@gmail.com>
2024-07-05 12:14:16 +0300

    Added class BackgroundFill

---
+++ src/main/java/et/telebof/types/BackgroundFill.java
@@ -0,0 +1,10 @@
+package et.telebof.types;
+
+import java.util.List;
+
+public class BackgroundFill {
+    public String type;
+    public Integer color, top_color, bottom_color, rotation_angle;
+    public List<Integer> colors;
+
+}
