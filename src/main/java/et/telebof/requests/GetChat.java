3f2226c9d63c65de3248b66d28eab54d239e043a
natanimn <natanimcoder@gmail.com>
2024-07-05 12:23:52 +0300

    Changed return type to the class ChatFullInfo

--- src/main/java/et/telebof/requests/GetChat.java
+++ src/main/java/et/telebof/requests/GetChat.java
@@ -1,15 +1,15 @@
 package et.telebof.requests;
 
 import et.telebof.Util;
-import et.telebof.types.Chat;
+import et.telebof.types.ChatFullInfo;
 
-public class GetChat extends AbstractBaseRequest<GetChat, Chat> {
+public class GetChat extends AbstractBaseRequest<GetChat, ChatFullInfo> {
     public GetChat(Object chatId, RequestSender requestSender) {
         super(chatId, requestSender, "getChat");
     }
 
     @Override
-    public Chat exec() {
-        return Util.parse(requestSender.makeRequest(this), Chat.class);
+    public ChatFullInfo exec() {
+        return Util.parse(requestSender.makeRequest(this), ChatFullInfo.class);
     }
 }
