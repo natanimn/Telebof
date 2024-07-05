a3ecdb2a887b1c69a5a9b23c1b5faacdcc628d82
natanimn <natanimcoder@gmail.com>
2024-07-05 12:26:10 +0300

    Added field text_entities

--- src/main/java/et/telebof/types/PollOption.java
+++ src/main/java/et/telebof/types/PollOption.java
@@ -1,9 +1,11 @@
 package et.telebof.types;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class PollOption implements Serializable {
     public String text;
     public Integer voter_count;
+    public List<MessageEntity> text_entities;
 
 }
